bd26f2e654c445d0f6196e0cc067a2cd85043090
Kaditya67 <140485509+Kaditya67@users.noreply.github.com>
2025-01-09 12:38:21 +0530

    Time: 3 ms (50.83%) | Memory: 12.2 MB (26.26%) - LeetSync

--- LeetCode/1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ LeetCode/1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
         set<string> result;
+        sort(words.begin(), words.end());
+        words.erase(unique(words.begin(), words.end()), words.end());
+        sort(words.begin(), words.end(), [](const string &a, const string &b) {
+            return a.size() < b.size();
+        });
         int n = words.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j) continue;
+        for(int i=0;i<n;++i){
+            for(int j=i+1;j<n;++j){ 
                 if(words[i].size() > words[j].size()) continue;
                 if(words[j].find(words[i]) != string::npos){
                     result.insert(words[i]);
+                    break;
                 }
             }
         }
