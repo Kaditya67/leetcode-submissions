f0dc14c29c81f2e4b2121a6752b76419dd8f2e3a
Kaditya67 <140485509+Kaditya67@users.noreply.github.com>
2025-01-24 23:24:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (75.24%) - LeetSync

--- LeetCode/125-valid-palindrome/valid-palindrome.cpp
+++ LeetCode/125-valid-palindrome/valid-palindrome.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        int n = s.length();
+    bool isPalindrome(string s) { 
         int i=0;
-        int j=n-1;
+        int j=s.length()-1;
         while(i<j){
             while(!isalnum(s[i]) && i<j) i++;
             while(!isalnum(s[j]) && i<j) j--;
