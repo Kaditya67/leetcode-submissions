575021b8292dce6127652dc01809c067d7f3967c
Kaditya67 <140485509+Kaditya67@users.noreply.github.com>
2025-02-17 21:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (76.08%) - LeetSync

--- LeetCode/1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ LeetCode/1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int numTilePossibilities(string tiles) {
-        unordered_set<string> sequences;
-        vector<bool> used(tiles.length(), false);
-
-        generateSequences(tiles, "", used, sequences);
-
-        return sequences.size() - 1;
-    }
-
-private:
-    void generateSequences(string& tiles, string current, vector<bool>& used,unordered_set<string>& sequences) { 
-        sequences.insert(current);
- 
-        for (int pos = 0; pos < tiles.length(); ++pos) {
-            if (!used[pos]) {
-                used[pos] = true;
-                generateSequences(tiles, current + tiles[pos], used, sequences);
-                used[pos] = false;
-            }
+void backtrack(unordered_map<char,int>&mapi,int &count){
+    for(auto&entry:mapi){
+        if(entry.second>0){
+            entry.second--;
+            count++;
+            backtrack(mapi,count);
+            entry.second++;
         }
+    }
+}
+    int numTilePossibilities(string tiles) {
+        unordered_map<char,int>mapi;
+        for(int i=0;i<tiles.size();i++){
+            mapi[tiles[i]]++;
+        }
+        int count=0;
+        backtrack(mapi,count);
+        return count;
     }
 };
