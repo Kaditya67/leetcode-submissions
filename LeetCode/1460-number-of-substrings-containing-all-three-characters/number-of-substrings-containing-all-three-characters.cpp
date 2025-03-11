f61390f633b614fbac70731a10309c8b68e7d662
Kaditya67 <140485509+Kaditya67@users.noreply.github.com>
2025-03-11 20:19:48 +0530

    Time: 4 ms (82.48%) | Memory: 10.9 MB (81.05%) - LeetSync

--- LeetCode/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ LeetCode/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<int> ch = vector<int>(3, 0); // To store frequency of 'a', 'b', 'c'
+    vector<int> ch = vector<int>(3, 0); 
 
     bool isValid() {
         int cnt = 0;
         for (int num : ch) {
             if (num > 0) cnt++;
         }
-        return cnt == 3; // Must have exactly 3 unique characters
+        return cnt == 3; 
     }
 
     int numberOfSubstrings(string s) {
