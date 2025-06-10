3ec2486ed176c7a6553a8de6774595810e9e00ce
Kaditya67 <140485509+Kaditya67@users.noreply.github.com>
2025-06-10 21:21:21 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (33.36%) - LeetSync

--- LeetCode/229-majority-element-ii/majority-element-ii.cpp
+++ LeetCode/229-majority-element-ii/majority-element-ii.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int count1=0,count2=0;
+        int candidate1=-2,candidate2=-1;
+        vector<int>res;
         int n = nums.size();
-        unordered_map<int,int> mp;
-
-        for(int &num:nums){
-            mp[num]++;
-        }
-
-        vector<int> result;
         int threshold = n/3;
-        for(auto &e:mp){
-            if(e.second > threshold){
-                result.push_back(e.first);
+        for(int &num:nums){
+            if(count1==0 && num!=candidate2){
+                count1++;
+                candidate1=num;
+            }
+            else if(count2==0 && num!=candidate1){
+                count2++;
+                candidate2=num;
+            }
+            else if(num==candidate1){
+                count1++;
+            }
+            else if(num==candidate2){
+                count2++;
+            }else{
+                count1--;
+                count2--;
             }
         }
-        return result;
+        // Now we have all good elements stored in candidate variables
+        // Count the real number of occurrences of the candidate
+
+        count1=0,count2=0;
+        for(int &num:nums){
+            if(num==candidate1) count1++;
+            else if(num==candidate2) count2++;
+        }
+
+        if(count1>threshold) res.push_back(candidate1);
+        if(count2>threshold) res.push_back(candidate2);
+        return res;
     }
 };
