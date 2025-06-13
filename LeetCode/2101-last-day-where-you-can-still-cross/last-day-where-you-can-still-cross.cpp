8510463a00cde5bd0a1c08a89ea5062f2e4b6c0d
Kaditya67 <140485509+Kaditya67@users.noreply.github.com>
2025-06-13 09:48:40 +0530

    Time: 203 ms (61.71%) | Memory: 180.3 MB (58.10%) - LeetSync

---
+++ LeetCode/2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int ROW,COL;
+    vector<vector<int>> directions = { 
+        {1, 0},   // down
+        {-1, 0},  // up
+        {0, 1},   // right
+        {0, -1}   // left
+    };
+
+    bool dfs(vector<vector<int>>& grid,int i,int j){
+        // Case
+        if(i<0 || i>=ROW || j<0 || j>=COL || grid[i][j]==1){
+            return false;
+        }
+        if(i==ROW-1) return true;   // End Row reached --> Success
+
+        grid[i][j]=1;       // Mark Visited cell
+        
+        // Check for next cells in all directions and DFS for them
+        for(vector<int> &dir:directions){
+            if(dfs(grid, i+dir[0], j+dir[1])) return true;
+        }
+
+        return false;
+    }
+    bool canCross(vector<vector<int>>& cells,int day){
+        vector<vector<int>>grid(ROW,vector<int>(COL));
+
+        for(int i=0;i<=day;++i){
+            int r=cells[i][0]-1;
+            int c=cells[i][1]-1;
+
+            grid[r][c]=1;
+        }
+
+        for(int j=0;j<COL;j++){
+            if(grid[0][j]==0 && dfs(grid,0,j)){ // start from 1st row and move
+                return true;
+            }
+        }
+        return false;
+    }
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        ROW = row;
+        COL = col;
+        vector<vector<int>>grid(row,vector<int>(col,0)); // Initialize 2D matrix with 0
+
+        int n = cells.size();
+        int l=0;
+        int r=n-1;
+        int lastDay=0;
+
+        while(l<=r){
+            int mid = l+(r-l)/2;
+
+            if(canCross(cells,mid)){
+                l = mid+1;
+                lastDay = mid+1;    // Index+1 gives day number
+            }else{
+                r = mid-1;
+            }
+        }
+        return lastDay;
+    }
+};
